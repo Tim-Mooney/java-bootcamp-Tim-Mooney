8c48e7bbfba2b08a0fdae34d8d88dd47b62426a1
Tim-Mooney <92114858+Tim-Mooney@users.noreply.github.com>
2026-07-21 11:17:34 -0400

    rough lab 1 comb through

---
+++ examples/jvm-compilation-lab/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    // JVM entry point when you run: java HelloWorld
+    public static void main(String[] args) {
+        // Print one line to the terminal
+        System.out.println("Hello, JVM!");
+    }
+}
